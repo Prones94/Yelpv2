56e656fcfc61920a9c963166f5fabc801305bbba
Ian Rones <ronesp.85@gmail.com>
2020-01-10 03:28:31 -0800

    [add] initial commit

---
+++ app.js
@@ -0,0 +1,92 @@
+const express = require('express'),
+    app = express(),
+    port = 3000,
+    request = require('request'),
+    bodyParser = require('body-parser'),
+    mongoose = require("mongoose"),
+    Schema = mongoose.Schema;
+
+mongoose.connect("mongodb://localhost:27017/yelp", {
+    useNewUrlParser:true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
+//Schema Setup
+var campgroundSchema = new Schema({
+    name:String,
+    image:String,
+    description:String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//     {
+//         name: 'Granite Hill',
+//         image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg",
+//         description: 'This is a huge granite hill campground, with zero bathrooms but beautiful landscapes'
+//     }, 
+//     function(err,campground){
+//         if(err){
+//             console.log('Error!', err)
+//         } else {
+//             console.log('Campground Created!')
+//             console.log(campground)
+//         }
+//     })
+
+app.get('/', (req,res) => {
+    res.render("home");
+});
+// INDEX ROUTE - show all campgrounds
+app.get('/campgrounds', (req,res) => {
+    // Get all campgrounds from DB
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err)
+        } else {
+            res.render("index", {campgrounds: allCampgrounds})
+        }
+    });
+    // res.render('campgrounds',{campgrounds:campgrounds});
+});
+
+// CREATE ROUTE - add new campground to DB
+app.post('/campgrounds', (req,res) => {
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var newCampGround = {name:name,image:image, description: desc};
+    // Create a new campground and save to DB
+    Campground.create(newCampGround, function(err, newlyCreated){
+        if(err){
+            console.log('Error!', err)
+        } else {
+            res.redirect('/campgrounds')
+        }
+    })
+});
+
+// NEW - show form to create new campground
+app.get('/campgrounds/new', (req,res) => {
+    res.render('new.ejs')
+})
+// SHOW - shows more info about one campground
+app.get('/campgrounds/:id', (req,res) => {
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log('Error!', err)
+        } else {
+            res.render('show', {campground: foundCampground})
+        }
+    });
+})
+
+
+app.listen(port, () => {
+    console.log(`YelpCamp V2 is now runnning on http://localhost:${port}/`)
+})
---
+++ objectives.md
@@ -0,0 +1,25 @@
+# YelpCamp Version 2
+
+## Add Mongoose
+
+- install and configure mongoose
+- Setup campground model
+- Use campground model inside of our routes!
+
+# Show Page
+* Review the RESTful routes we've seen
+* Add description to our campground model
+* Show db.collection.drop()
+* Add a show route template
+
+# Restful Routes
+
+name url verb description
+============================================
+## Index  /dogs      GET  Display a list of all dogs
+
+## New    /dogs/new  GET  Displays form
+
+## Create /dogs      POST Add new dog to DB
+
+## Show   /dogs/:id  GET Shows info about one dog
